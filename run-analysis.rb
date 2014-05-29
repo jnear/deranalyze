e2b840812d2859fdac642c63d981c7e966c51523
Ubuntu <ubuntu@auto-csail-precise.csail.mit.edu>
2014-05-29 16:34:49 -0400

    added first script version

---
+++ run-analysis.rb
@@ -0,0 +1,54 @@
+
+if ARGV.length != 1 then
+  abort("Error: requires one argument")
+end
+
+github_address = ARGV[0]
+app_dir = github_address.split("/").last.split(".").first
+
+# clone the repo
+puts "Downloading #{github_address} into #{app_dir}..."
+system ("git clone #{github_address}")
+Dir.chdir app_dir
+
+# append stuff to the gemfile
+puts "Modifying the gemfile..."
+no_sqlite = open('Gemfile', 'r') { |f| f.grep(/sqlite3/).empty? }
+
+
+open('Gemfile', 'a') { |f|
+  f.puts <<EOS
+
+# Added by derailer analysis
+gem 'derailer', :git => 'https://github.com/jnear/derailer.git'
+gem 'method_source', "~>0.8.3", :git => 'https://github.com/aleksandarmilicevic/method_source.git'
+gem 'sdg_utils', :git => 'https://github.com/jnear/sdg_utils.git'
+
+EOS
+
+  if no_sqlite then
+    f.puts "gem 'sqlite3'"
+  else
+    f.puts "# sqlite already present"
+  end
+  f.puts ""
+}
+
+# make a new gemset for this app
+puts "Making a new gemset for #{app_dir}..."
+system "rvm gemset create #{app_dir}"
+system "rvm gemset use #{app_dir}"
+
+# install gems
+puts "Running bundle install..."
+system "bundle install"
+
+# install our database.yml
+puts "Installing database.yml..."
+system "cp ../database.yml ./config/database.yml"
+
+# now init the database
+puts "Initializing database (rake db:migrate)..."
+system "bundle exec rake db:migrate"
+
+Dir.chdir ".."
